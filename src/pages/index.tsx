fdc5eea3d160ef49ad92453a55cd2a3791f57e5d
Takuya Nishimoto <nishimotz@gmail.com>
2023-03-24 10:36:48 +0900

    CC BY の代替テキストを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ const Home: NextPage = () => {
         <FooterP>
           <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
             <img
-              alt="クリエイティブ・コモンズ・ライセンス"
+              alt="CC BY"
               style={{ borderWidth: 0 }}
               src="https://i.creativecommons.org/l/by/4.0/88x31.png"
             />
